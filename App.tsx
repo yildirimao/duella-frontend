90097091c218f3ce2a37c3a1b13b605baf338adb
tolga792 <tastolgahantas@gmail.com>
2026-02-22 21:40:15 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> nasıl? </Text>
+      <Text> hey corç </Text>
       <StatusBar style="auto" />
     </View>
   );
